8042bb3b673155f7ce8ac071f9f78adba0d3427e
jjoylee <ekdnlt1995@gmail.com>
2018-07-14 16:28:48 +0900

    toDoList - add, delete, toggleDone, toggleAll

--- Angularjs/jeongheeToDo/js/toDoList.js
+++ Angularjs/jeongheeToDo/js/toDoList.js
@@ -1,6 +1,4 @@
 
-var toDoModel = [];
-
 var toDoApp = angular.module("toDoApp",[]);
 
 // toDoApp.directive('submitTodo', function(){
@@ -17,10 +15,66 @@ var toDoApp = angular.module("toDoApp",[]);
 // });
 
 toDoApp.controller("ToDoCtrl", function($scope){
-  $scope.toDo = toDoModel;
+  $scope.toDo = [];
 
   $scope.addNewToDo = function(event, todoText){
     if(event.keyCode !== 13) return;
-    toDoModel.push()
+    $scope.toDo.push({
+      id : uuidGenerator(),
+      text : todoText,
+      done : false,
+    });
+    event.currentTarget.value = "";
+  };
+
+  $scope.deleteToDo = function(id){
+    var idx = getToDoIdxById(id);
+    if(idx) delete $scope.toDo[idx];
+    $scope.toDo = $scope.toDo.filter(value => value !== undefined);
+  };
+
+  var getToDoIdxById = function(id){
+    for(var idx in $scope.toDo){
+      if($scope.toDo[idx].id === id)
+        return idx;
+    }
+  };
+
+  $scope.toggleDone = function(id){
+    var idx = getToDoIdxById(id);
+    if(idx) $scope.toDo[idx].done = !$scope.toDo[idx].done;
+  };
+
+  $scope.checkDivStyle = function(doneStatus){
+    return (doneStatus === true)? "toDo_check done" : "toDo_check";
+  };
+
+  $scope.checkStyle = function(doneStatus){
+    return (doneStatus === true)? "far fa-check-circle" : "far fa-circle";
+  };
+
+  $scope.textDivStyle = function(doneStatus){
+    return (doneStatus === true) ? "toDo_text doneText" : "toDo_text";
+  };
+
+  $scope.toggleAllStyle = "toggleAll";
+
+  $scope.toggleAll = function(){
+    $scope.toggleAllStyle = ($scope.toggleAllStyle === "toggleAll") ? "" : "toggleAll";
+    var done = ($scope.toggleAllStyle === "toggleAll") ? false : true;
+    setAllToDoDone(done);
+  };
+
+  var setAllToDoDone = function(done){
+    for(var idx in $scope.toDo){
+      $scope.toDo[idx].done = done;
+    }
   }
+
+  var uuidGenerator = function(){
+    function s4() {
+      return ((1 + Math.random()) *  0x10000 | 0).toString(16).substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+  };
 });
