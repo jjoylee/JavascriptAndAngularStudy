b6d12a50e5cfcb8782df1beaceabe78a3569dbbd
jjoylee <ekdnlt1995@gmail.com>
2018-07-25 14:36:48 +0900

    ProAngularJS Chapter9

---
+++ Angularjs/angularjsStudy/chapter9/controllers.js
@@ -0,0 +1,8 @@
+var controllersModule = angular.module("exampleApp.Controllers", []);
+controllersModule
+.controller("dayCtrl", function($scope, days){
+  $scope.day = days.today;
+})
+.controller("tomorrowCtrl", function($scope,days){
+  $scope.day = days.tomorrow;
+});
