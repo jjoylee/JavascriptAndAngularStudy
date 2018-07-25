edd59e4225d8275c78a02abad97597d4206b6abc
jjoylee <ekdnlt1995@gmail.com>
2018-07-25 16:01:43 +0900

    ProAngularJS Chapter10

--- Angularjs/server.js
+++ Angularjs/server.js
@@ -1,4 +1,4 @@
 var connect = require('connect');
 var path = require('path');
-var app = connect().use(connect.static('./angularjsStudy/deployd/sportsstore'));
+var app = connect().use(connect.static('./angularjsStudy/examples'));
 app.listen(5000);
